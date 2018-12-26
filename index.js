9740c98057a32adc14dc68d7455b6eb6d6d4f2a8
Imran Chowdhury <imran@imranc.io>
2018-12-26 20:45:03 +1100

    Added stripe charge logic

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 // doesn't export anything; need to consider order
 require("./models/User");
@@ -15,6 +16,10 @@ const app = express();
 // middlewares do some pre-processing on the request / modify them
 // before passing off to route handlers
 
+// parses json payloads from POST/PUT/PATCH requests
+// and adds it to the request.body of incoming request
+app.use(bodyParser.json());
+
 // cookie-session encrypts / decrypts cookies
 app.use(
   cookieSession({
@@ -23,11 +28,12 @@ app.use(
     keys: [keys.cookieKey]
   })
 );
-
+// passport simplifies oauth flow
 app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
