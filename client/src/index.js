d158078b00b60a0917e2eeb43f0015bfb06e88e1
Imran Chowdhury <imran@imranc.io>
2018-12-26 11:23:40 +1100

    Added boilerplate for Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+// create Redux store
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  // connect Redux store to React using Provider component
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
