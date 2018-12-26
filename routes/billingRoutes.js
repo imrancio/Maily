be9abe8aa6fee61c9495016ada276546682e65d2
Imran Chowdhury <imran@imranc.io>
2018-12-26 21:05:04 +1100

    Added logged in check before charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,12 +3,19 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post("/api/stripe", async (req, res) => {
+    if (!req.user) {
+      return res.status(401).send({ error: "You must log in!" });
+    }
+
     const charge = await stripe.charges.create({
       amount: 500,
       currency: "usd",
       description: "$5 for 5 credits",
       source: req.body.id
     });
-    console.log(charge);
+
+    req.user.credits += 5;
+    const user = await req.user.save();
+    res.send(user);
   });
 };
