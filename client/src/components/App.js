6d3a00792e82923ddd2ab4c4de2388c65884b41f
Imran Chowdhury <imran@imranc.io>
2018-12-26 15:00:27 +1100

    Completed header logo redirects

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header";
+import Landing from "./Landing";
 import * as actions from "../actions";
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   componentDidMount() {
