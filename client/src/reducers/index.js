42ef68cc792878e4ac5e68d43685a41a1ef0aafa
Imran Chowdhury <imran@imranc.io>
2018-12-28 00:07:54 +1100

    Wired up surveysReducer / fetchSurveys action creator

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from "redux";
-import { reducer as reduxForm } from "redux-form";
+import { reducer as formReducer } from "redux-form";
 import { authReducer } from "./authReducer";
+import surveysReducer from "./surveysReducer";
 
 export default combineReducers({
   // auth state produced by authReducer
   auth: authReducer,
   // form state produced by reduxForm
-  form: reduxForm
+  form: formReducer,
+  // surveys state produced by surveysReducer
+  surveys: surveysReducer
 });
