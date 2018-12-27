4436f3cd3d126e9791d27bef918b467160123cb2
Imran Chowdhury <imran@imranc.io>
2018-12-27 18:31:09 +1100

    Added logic to switch between SurveyForm / SurveyFormReview

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,14 +1,26 @@
 // SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from "react";
 import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends Component {
-  render() {
+  // babel will initialise state in constructor
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return <SurveyFormReview />;
+    }
+
     return (
-      <div>
-        <SurveyForm />
-      </div>
+      <SurveyForm
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
+      />
     );
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
